ac80e7129c2da750c86f7f5f0fa497b3e7825565
Kate Hart <codehearts@users.noreply.github.com>
2013-08-15 18:33:41 -0500

    Rewrote portfolio helper to fetch entries by type

--- lib/portfolio_helper.rb
+++ lib/portfolio_helper.rb
@@ -1,16 +1,40 @@
 module Portfolio
 
-  def portfolios
-    @items.select { |item| item[:kind] == 'portfolio' }
-  end
+	# Content directory where portfolio entries are stored
+	@@portfolio_dir = '/data/portfolio/'
+	# Content subdirectory where in-progress portfolio entries are stored
+	@@in_progress_dir = @@portfolio_dir + '/in-progress/'
+	# Content subdirectory where scrapped portfolio entries are stored
+	@@scraps_dir = @@portfolio_dir + '/scraps/'
+	# Cache of known portfolio items
+	# Hash with keys 'all', 'completed', 'in-progress', and 'scraps'
+	@@known_portfolio_entries = nil
 
-  def sorted_portfolios
-    portfolios.sort_by { |p| attribute_to_time(p[:date]) }.reverse
-  end
+	# Caches all portfolio items
+	def cache_portfolio_entries
+		if @@known_portfolio_entries == nil
+			all = @items.select { |item| item.identifier.start_with? @@portfolio_dir }
+			ongoing = all.select { |item| item.identifier.start_with? @@in_progress_dir }
+			scraps = all.select { |item| item.identifier.start_with? @@scraps_dir }
+			completed = all.select { |item| not (ongoing.include? item or scraps.include? item) }
 
-  def portfolio_image_url(item, type)
-    '/images/portfolio/' + item[:image_id] + '_' + type + '.jpg'
-  end
+			@@known_portfolio_entries = {
+				'all'         => all,
+				'completed'   => completed,
+				'in-progress' => ongoing,
+				'scraps'      => scraps,
+			}
+		end
+	end
+
+	# Gets all portfolio items of the given type sorted by their `date` value
+	# Valid types are 'all', 'completed', 'in-progress', and 'scraps'.
+	def portfolio_items(type='completed')
+		# Cache known portfolio entries
+		cache_portfolio_entries
+
+		@@known_portfolio_entries[type].sort_by { |item| attribute_to_time(item[:date]) }.reverse
+	end
 
 end
 
