893cca248814033268339ef0ee5ab48cdab27563
Kate Hart <codehearts@users.noreply.github.com>
2013-08-25 22:21:51 -0500

    Added another site config variable

--- lib/globals.rb
+++ lib/globals.rb
@@ -6,6 +6,7 @@ $images = 'media/images/'
 $portfolio_images = $images_dir + 'portfolio/'
 
 $partial_layouts = 'partials/'
+$profile_layouts = $partial_layouts + 'profiles/'
 $portfolio_layouts = $partial_layouts + 'portfolio/'
 $in_progress_layouts = $portfolio_layouts + 'in-progress/'
 $scraps_layouts = $portfolio_layouts + 'scraps/'
