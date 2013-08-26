ac4a21f34568e5fcba33c92fb8ff43de4f82dcb4
Kate Hart <codehearts@users.noreply.github.com>
2013-08-25 22:01:40 -0500

    Added more global site config variables, applied them throughout

--- lib/globals.rb
+++ lib/globals.rb
@@ -1,3 +1,11 @@
 # Global site configuration variables
 
-$contacts = ['email', 'twitter', 'github'];
+$contacts = ['email', 'twitter', 'github']
+
+$images = 'media/images/'
+$portfolio_images = $images_dir + 'portfolio/'
+
+$partial_layouts = 'partials/'
+$portfolio_layouts = $partial_layouts + 'portfolio/'
+$in_progress_layouts = $portfolio_layouts + 'in-progress/'
+$scraps_layouts = $portfolio_layouts + 'scraps/'
