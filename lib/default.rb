2f40f3f3f8bc4f8d2ed93ad26c135397839cdcff
Kate Hart <codehearts@users.noreply.github.com>
2014-07-11 13:23:23 -0400

    Moved gem requirements to lib/defaults.rb

--- lib/default.rb
+++ lib/default.rb
@@ -1,3 +1,12 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
-require 'nanoc/filters/image_compressor'
+
+# Prevent loading gems multiple times when using an automatic compiler
+unless defined? LOADED_DEFAULT_CONFIG
+	LOADED_DEFAULT_CONFIG = true
+
+	require 'compass'
+	Compass.add_project_configuration 'compass_config.rb' 
+
+	require 'nanoc/filters/image_compressor'
+end
