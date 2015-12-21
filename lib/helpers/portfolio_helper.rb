fdfa1dd2f2894dc49ca3bc08bdd66412dcae37db
Kate Hart <codehearts@users.noreply.github.com>
2015-12-20 20:48:00 -0600

    Cleaned up Portfolio code

--- lib/helpers/portfolio_helper.rb
+++ lib/helpers/portfolio_helper.rb
@@ -1,38 +1,39 @@
 module Portfolio
 
-	# Content directories where portfolio entries are stored
-	@@portfolio_dir   = '/portfolio/'
-	@@scraps_dir      = @@portfolio_dir + 'scraps/'
-	@@disabled_dir    = @@portfolio_dir + 'disabled/'
-	# Cache of known portfolio items
-	# Hash with keys 'all', 'completed', and 'scraps'
-	@@known_portfolio_entries = nil
+  # Content directories where portfolio entries are stored
+  @@portfolio_dir = '/portfolio/'
+  @@scraps_dir    = @@portfolio_dir + 'scraps/'
+  @@disabled_dir  = @@portfolio_dir + 'disabled/'
+  @@portfolio_entry_cache = nil
 
-	# Caches all portfolio items
-	def cache_portfolio_entries
-		if @@known_portfolio_entries == nil
-			all = @items.select { |item| item.identifier.start_with? @@portfolio_dir }
-			scraps = all.select { |item| item.identifier.start_with? @@scraps_dir }
-			disabled = all.select { |item| item.identifier.start_with? @@disabled_dir }
-			completed = all.select { |item| not ((scraps.include? item) or (disabled.include? item)) }
+  # Returns all portfolio items as a Hash with
+  # the following keys:
+  #   'all', 'completed', and 'scraps'
+  #
+  def all_portfolio_entries
+    if @@portfolio_entry_cache.nil?
+      all       = @items.select { |item| item.identifier.to_s.start_with? @@portfolio_dir }
+      scraps    = all.select { |item| item.identifier.to_s.start_with? @@scraps_dir }
+      disabled  = all.select { |item| item.identifier.to_s.start_with? @@disabled_dir }
+      completed = all.select { |item| not ((scraps.include? item) or (disabled.include? item)) }
 
-			@@known_portfolio_entries = {
-				'all'         => all,
-				'completed'   => completed,
-				'scraps'      => scraps,
-				'disabled'    => disabled,
-			}
-		end
-	end
+      @@portfolio_entry_cache = {
+        'all'       => all,
+        'completed' => completed,
+        'scraps'    => scraps,
+        'disabled'  => disabled,
+      }
+    end
 
-	# Gets all portfolio items of the given type sorted by their `date` value.
-	# Valid type arguments are 'all', 'completed', 'and 'scraps'.
-	def portfolio_items(type='completed')
-		# Cache known portfolio entries
-		cache_portfolio_entries
+    return @@portfolio_entry_cache
+  end
 
-		@@known_portfolio_entries[type].sort_by { |item| attribute_to_time(item[:date]) }.reverse
-	end
+  # Returns all portfolio items of the given type sorted by their `date` value.
+  # Valid type arguments are 'all', 'completed', 'and 'scraps'.
+  #
+  def portfolio_items(type = 'completed')
+    all_portfolio_entries[type].sort_by { |item| attribute_to_time(item[:date]) }.reverse
+  end
 
 end
 
