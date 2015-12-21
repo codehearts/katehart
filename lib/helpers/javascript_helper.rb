87f3f0f554f91db378e54072e6a8638de733a70a
Kate Hart <codehearts@users.noreply.github.com>
2015-12-20 18:21:47 -0600

    Cleaned up JavascriptConcatenator

--- lib/helpers/javascript_helper.rb
+++ lib/helpers/javascript_helper.rb
@@ -1,22 +1,27 @@
 module JavascriptConcatenator
 
-	# Content directory where JavaScript is stored
-	@@js_dir  = '/js/'
+  # Content directory where JavaScript is stored
+  @@js_dir  = '/js/'
+
+  # This file will store the concated JS
   @@js_file = @@js_dir + 'scripts.js'
 
-	def concatenate_js()
-		scripts = @items.select { |item|
-        filename = item.identifier.to_s
-        filename.start_with? @@js_dir and filename != @@js_file
+  # Returns the concatenation of all JS items,
+  # excluding the concatentation file (@@js_file).
+  #
+  def concatenate_js()
+    # Collect a list of all JS items, excluding the concatenation file
+    scripts = @items.select { |item|
+      filename = item.identifier.to_s
+      filename.start_with? @@js_dir and filename != @@js_file
     }
-		concatenated_js = []
 
-		for script in scripts
-			concatenated_js << script.compiled_content
-		end
+    # Get the compiled content for all JS
+    scripts.map! { |s| s.compiled_content }
 
-		concatenated_js.join("\n")
-	end
+    # Join all file contents into a single string
+    return scripts.join("\n")
+  end
 end
 
 include JavascriptConcatenator
